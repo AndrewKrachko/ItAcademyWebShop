3626ebaccc3142cf8b26e8c3ec2f83daa2d1f434
AndrewKrachko <AndrewKrachko@gmail.com>
2021-01-07 22:53:02 +0300

    Added blank class for Linq to Sql implementation

---
+++ MsSqlDbDataLinqProvider/LinqDataProvider.cs
@@ -0,0 +1,39 @@
+﻿using ItAcademyWebShop.Items.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace MsSqlDb.LinqDataProvider
+{
+    public class LinqDataProvider : IRepository
+    {
+        public IEnumerable<ICategory> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IItem> GetCategoryItems(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IItem GetItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IItem> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IItem> GetItems(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IItem> GetItems(string name, string category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
